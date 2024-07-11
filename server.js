f9780931cdfc6f0e7b8b82295d0b94c9b96a2327
Pascal George <tamunoemi@gmail.com>
2024-07-11 06:13:39 -0400

    add CORS middleware

--- server.js
+++ server.js
@@ -6,6 +6,24 @@ import { body, validationResult } from "express-validator";
 
 const app = express();
 
+// Middleware to handle CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, Content-Type, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204); // No Content
+  }
+  next();
+});
+
 // Middleware to parse JSON bodies
 app.use(express.json());
 
